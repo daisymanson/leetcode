9b3267171416654fe4b6039575e1af73b4ca6068
daisywang <daisywang265@gmail.com>
2022-01-17 14:35:41 +0800

    382.Linked List Random Node

--- leedcode/leetcode.go
+++ leedcode/leetcode.go
@@ -1,6 +1,10 @@
 package leetcode
 
-// 27.
+import (
+	"math/rand"
+)
+
+// 27. RemoveElement
 func removeElement(nums []int, val int) int {
 	n := len(nums)
 	for i := 0; i < n; {
@@ -14,7 +18,7 @@ func removeElement(nums []int, val int) int {
 	return n
 }
 
-// 2.
+// 2. TwoSum
 func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
 	for k, v := range nums {
@@ -25,4 +29,36 @@ func twoSum(nums []int, target int) []int {
 		m[v] = k
 	}
 	return nil
+}
+
+// 382.  Linked List Random Node
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type Solution struct {
+	count int
+	head  *ListNode
+	pool  []int
+}
+
+func Constructor(head *ListNode) Solution {
+	var s Solution
+	s.head = head
+	return s
+}
+
+func (this *Solution) GetRandom() int {
+	r := rand.Intn(this.count + 1)
+	this.count++
+	if r == this.count-1 && this.head != nil {
+		t := this.head
+		this.head = this.head.Next
+		this.pool = append(this.pool, t.Val)
+		return t.Val
+	} else {
+		return this.pool[r%len(this.pool)]
+	}
+
 }
